8331beb782e17c439320cd4a304a895832c92746
dangquangkha <126367262+dangquangkha@users.noreply.github.com>
2025-12-16 16:18:59 +0700

    add bat

--- Robot_School.bat
+++ Robot_School.bat
@@ -1,3 +1,15 @@
 @echo off
-python main.py
+:: Lệnh này tự động chuyển hướng về thư mục chứa file này 
+:: (Tức là C:\Users\Administrator\Documents\GitHub\robot_school)
+cd /d "%~dp0"
+
+echo Dang khoi dong Robot School...
+echo --------------------------------
+
+:: Gọi Python từ thư mục .venv nằm ngay bên cạnh
+:: Lệnh này sửa lỗi "ModuleNotFoundError: No module named 'kivy'"
+".venv\Scripts\python.exe" main.py
+
+:: Giữ màn hình lại để xem nếu có lỗi
+echo.
 pause
